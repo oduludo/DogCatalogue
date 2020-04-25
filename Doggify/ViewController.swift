67a5fefb606b9db347b5ee898e1504e1bb314b9f
oduludo <ludo@vanorden.eu>
2020-04-25 09:17:13 +0200

    Initial Commit

---
+++ Doggify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Doggify
+//
+//  Created by Ludo van Orden on 25/04/2020.
+//  Copyright © 2020 Ludo van Orden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
